89065868a2ff5eff2ac4555683a314a46af96b93
Marco <mistermarco91@hotmail.it>
2025-05-22 18:50:38 +0200

    update

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM php:8.3-apache
-COPY . /var/www/html/
+COPY . :/var/www/html
 
 # Installa estensioni necessarie
 RUN docker-php-ext-install pdo pdo_mysql
--- src/config/constants.php
+++ src/config/constants.php
@@ -1,8 +1,15 @@
 <?php
 // Costanti di configurazione del database
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASS', getenv('DB_PASS'));
-// Se hai una password mettila qui, altrimenti lascialo vuoto
+$host = getenv('DB_HOST') ?: 'db';
+$dbname = getenv('DB_NAME') ?: 'esportify_db';
+$user = getenv('DB_USER') ?: 'esport';
+$pass = getenv('DB_PASS') ?: 'esport123'; // Se hai una password mettila qui, altrimenti lascialo vuoto
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    die("Errore di connessione al database: " . $e->getMessage());
+}
 ?>
